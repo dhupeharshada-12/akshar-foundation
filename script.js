8ca9013f1b8ab5c586d4ba05d781bdc7a6f25a2a
Harshada Dhupe <dhupeharshada@gmail.com>
2026-06-25 12:48:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log("Welcome to Akshar Foundation Website!");
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function(e){
+        e.preventDefault();
+
+        document.querySelector(
+            this.getAttribute("href")
+        ).scrollIntoView({
+            behavior:"smooth"
+        });
+    });
+});
